ddc306d3e9123b624209eeb04e540b3c1af1693d
marcoValencia <marco.valencia@outlook.com>
2025-08-14 21:45:04 -0600

    PyS7 v1.6

--- scripts/script.js
+++ scripts/script.js
@@ -78,3 +78,52 @@ function favorito(fav){
     }
   }
 }
+
+let save = document.querySelector(".popup__form_save-button");
+save.addEventListener("click", savePopup);
+function savePopup() {
+  const nameProfile = document.getElementById("name");
+  const name = document.querySelector(".profile__info_name");
+  name.textContent = nameProfile.value;
+
+  const activityProfile = document.getElementById("activity");
+  const activity = document.querySelector(".profile__info_activity");
+  activity.textContent = activityProfile.value;
+
+  const popup = document.getElementById("popup__container");
+  popup.style.visibility = "hidden";
+
+  const page = document.querySelector(".page");
+  page.style.opacity = 1;
+}
+
+// Busquemos el formulario en el DOM
+let formElement = // Utiliza el método querySelector()
+
+// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
+// no se enviará en ningún sitio todavía
+
+// Observa que el nombre de la función comienza con un verbo
+// y describe exactamente lo que hace la función
+function handleProfileFormSubmit(evt) {
+    // Esta línea impide que el navegador
+    // entregue el formulario en su forma predeterminada.
+    evt.preventDefault();
+    // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
+    // Lo explicaremos todo con más detalle después.
+
+    // Busquemos los campos del formulario en el DOM
+    let nameInput = // Utiliza el método querySelector()
+    let jobInput = // Utiliza el método querySelector()
+
+    // Obtén los valores de cada campo desde la propiedad de valor correspondiente
+
+    // Selecciona los elementos donde se introducirán los valores de los campos
+
+    // Inserta nuevos valores utilizando el textContent
+    // propiedad del método querySelector()
+}
+
+// Conecta el manipulador (handler) al formulario:
+// se observará el evento de entrega
+formElement.addEventListener('submit', handleProfileFormSubmit);
